ab446aa6194fd9a2230caa6ab1d2a544afb18483
Arnav Pushkarna <kakamishu2012@gmail.com>
2026-09-22 05:17:24 -0700

    Added a broad error handling statement in main loop

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def main(sms_queue: Queue, listener: Listener, command_parser: Parser):
 
             except Empty:
                 pass
+            except Exception as e:
+                print(f'Error occurred: \n{repr(e)}\n')
 
     except KeyboardInterrupt:
         print("\n[Main Program] Shutting down...")
