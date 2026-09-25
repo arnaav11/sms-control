094e1229495c2b3fd6906404c9cbc3f96bde0d84
Arnav Pushkarna <kakamishu2012@gmail.com>
2026-09-25 16:16:52 -0700

    Added tooling base class

--- tooling/tooling.py
+++ tooling/tooling.py
@@ -1,3 +1,13 @@
+from typing import Literal, Callable
+
+from plugins.plugin import Plugin
+
 class Tooling:
     def __init__(self):
+        self.cur_depth: int = 0
+        self.max_depth: int = 5
+        self.plugins: list[Plugin] = []
+        self.tools: dict[str, dict[Literal['method', 'description'], Callable[[str], str] | str]] = {}
+
+    def call_tools(self, tools: str, callback_cmd: str) -> str:
         pass
