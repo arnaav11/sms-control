e7b68058acaa5755595da6f28300645352546c0d
Arnav Pushkarna <kakamishu2012@gmail.com>
2026-09-25 16:19:46 -0700

    Implemented modular tooling

--- parsers/default_parser/default_parser.py
+++ parsers/default_parser/default_parser.py
@@ -2,10 +2,14 @@ import json
 
 from plugins.plugin import Plugin
 from parsers.parser import Parser
+from tooling.tooling import Tooling
 
 class DefaultParser(Parser):
-    def __init__(self, plugins: list[Plugin]):
-        super().__init__(plugins)
+    def __init__(self, plugins: list[Plugin], tooling: Tooling):
+        super().__init__()
+
+        self.plugins = plugins
+        self.tooling = tooling
 
     def parse_command(self, command_str: str) -> str:
         command_str = command_str.strip()
@@ -20,52 +24,24 @@ class DefaultParser(Parser):
         else:
             args = ' '.join(command[1:])
 
-        for i in range(len(self.plugins)):
-            plugin = self.plugins[i]
+        for idx in range(len(self.plugins)):
+            plugin = self.plugins[idx]
             if command[0] in plugin.get_tools():
                 method = plugin.get_tools()[command[0]]['method']
                 command_output = method(args)
                 
                 if plugin.tool_calling:
-                    return self.call_tool(command_output, plugin=plugin)
+                    return self.tooling.call_tools(command_output, callback_cmd=command[0])
 
                 return command_output
-
-    def call_tool(self, call_str: str, plugin: Plugin) -> str:
-        try:
-            tool_call = json.loads(call_str)
-        except json.decoder.JSONDecodeError:
-            print(f'Not a tool or error occured.\nTool: {call_str}\n')
-            return call_str
-
-        print('Received agentic tool calls: ')
-        print(tool_call)
-        print()
-
-        output = ''
-        callback_output = ''
-        for tool in tool_call:
-            if tool == 'respond':
-                output += f"{tool_call[tool]['args']}\n\n"
-
-            tool_call_str = f'/{tool} {tool_call[tool]['args']}'
-            tool_call_output = self.parse_command(tool_call_str)
-
-            if 'callback' in tool_call[tool] and tool_call[tool]['callback']:
-                callback_output += f'\n{tool_call_str} \n{tool_call_output}\n'
-
-        if callback_output:
-            callback_command = f'/{plugin.callback_method} {tool_call_output}'
-            output += f"\n{self.parse_command(callback_command)}\n"
-
-        return output
 
     
 if __name__ == '__main__':
     from config import *
 
     tester = DefaultParser(
-        plugins=plugins
+        plugins=plugins,
+        tooling=tooling
     )
 
     test_input = input('Enter command: ')
