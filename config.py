76e7d34403491557dc51fa995a4c0ea176abdc8e
Arnav Pushkarna <kakamishu2012@gmail.com>
2026-09-25 16:19:06 -0700

    modified config with shell commands, and default tooling

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ import os
 from queue import Queue
 from dotenv import load_dotenv
 
+from plugins.plugin import Plugin
 from plugins.llm_plugin.llm_plugin import LLMPlugin
 from plugins.shell_plugin.shell_plugin import ShellPlugin
 from plugins.search_plugin.search_plugin import SearchPlugin
 from listeners.sms_listener.sms_listener import SMSListener
 from parsers.default_parser.default_parser import DefaultParser
+from tooling.default_tooling.default_tooling import DefaultTooling
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 load_dotenv()
@@ -20,7 +22,11 @@ allowed_shell_commands = [
     'cd',
     'llama-server',
     'ollama',
-    'koboldcpp'
+    'koboldcpp',
+    'fastfetch',
+    'inxi',
+    'free',
+    'df'
 ]
 
 shell_plugin = ShellPlugin(
@@ -34,7 +40,7 @@ search_plugin = SearchPlugin(
 
 
 base_url = 'https://integrate.api.nvidia.com/v1'
-model = 'nvidia/nemotron-3-ultra-550b-a55b'
+model = 'z-ai/glm-5.3'
 
 reasoning = 'high'
 chat_save_folder = './chats'
@@ -44,10 +50,10 @@ max_tokens = 4096
 available_reasoning = ['none', 'low', 'medium', 'high']
 response_tool = {'respond': 'Respond to the user. Takes in the response text as the argument'}
 
-tool_messages = [
-    'Here are tools:',
-    'use them in json with {"tool_name": {"args": "tool_args", "callback": bool}, "tool_name"....} reply only in json. The callback is for whether you want the output of the tool call to be returned back to you. You can use as many tools as you want.'
-]
+# tool_messages = [
+#     'Here are tools:',
+#     'use them in json with {"tool_name": {"args": "tool_args", "callback": bool}, "tool_name"....} reply only in json. The callback is for whether you want the output of the tool call to be returned back to you. You can use as many tools as you want.'
+# ]
 
 llm_plugin = LLMPlugin(
     base_url=base_url,
@@ -58,13 +64,16 @@ llm_plugin = LLMPlugin(
     save_folder=chat_save_folder,
     system_message=system_message,
     available_tools = {},
-    tool_messages=tool_messages,
     response_tool=response_tool
 )
 
 
-plugins = [llm_plugin, shell_plugin, search_plugin]
-parser = DefaultParser(plugins=plugins)
+plugins: list[Plugin] = [llm_plugin, shell_plugin, search_plugin]
+
+max_depth = 5
+tooling = DefaultTooling(plugins, max_depth)
+
+parser = DefaultParser(plugins=plugins, tooling=tooling)
 
 tools = {}
 for plugin in plugins:
